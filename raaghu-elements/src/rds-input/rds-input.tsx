566ec1605f3ca400b078e2ca7a0e03c30d2891e8
Nitin Sonawane <nitin.sonawane@waiin.com>
2024-12-04 18:09:43 +0530

    Commit Changes for bug 1106

--- raaghu-elements/src/rds-input/rds-input.tsx
+++ raaghu-elements/src/rds-input/rds-input.tsx
@@ -200,7 +200,7 @@ const RdsInput = React.forwardRef<HTMLInputElement, RdsInputProps>(
           {props.showTitle && (
             <label
               htmlFor={props.id}
-              className={`text-capitalize mt-2 form-label ${fontWeight}`}
+              className={`text-capitalize form-label ${fontWeight} ${props.id === 'address-input' ? 'mt-1' : 'mt-2'}`}
             >
               {props.label}
               {(props.required || props.validatonPattern) && (
